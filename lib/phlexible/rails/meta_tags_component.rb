2061a8f48189ff045632ce71f9096cf1a0dd674c
Joel Moss <joel@developwithstyle.com>
2024-02-01 19:51:02 +0000

    fix: Use view_assigns to fetch meta_tags

--- lib/phlexible/rails/meta_tags_component.rb
+++ lib/phlexible/rails/meta_tags_component.rb
@@ -4,7 +4,7 @@ module Phlexible
   module Rails
     class MetaTagsComponent < Phlex::HTML
       def template
-        controller.instance_variable_get(:@meta_tags)&.each do |name, content|
+        helpers.controller.view_assigns['meta_tags']&.each do |name, content|
           meta name: name, content: content.is_a?(String) ? content : content.to_json
         end
       end

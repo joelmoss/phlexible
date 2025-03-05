b802b0d4364113f814b586d25ed7e96965171eae
Henning Koch <henning.koch@makandra.de>
2025-03-05 15:57:36 +0100

    Fix deprecation warning from engine mount in tests

--- fixtures/rails_helper.rb
+++ fixtures/rails_helper.rb
@@ -7,4 +7,5 @@ Combustion.path = 'fixtures/dummy'
 Combustion.initialize! :action_controller do
   config.load_defaults 7.0
   config.autoload_paths << "#{root}/app"
+  config.active_support.to_time_preserves_timezone = :zone
 end
